d5e7b4718af9dbcdd807f860ec40b60556bcf248
Creminin <n54318879@gmail.com>
2023-12-29 11:17:14 -0300

    Making modifications for new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Flex } from "@chakra-ui/react"
+import { ModalForm } from "./assets/components/ModalForm"
+
 function App() {
 
   return (
-    <>
-    </>
+    <Flex h='100vh' bg='MyBgColor' color='white' justifyContent='center' alignItems='center'>
+      <ModalForm />
+    </Flex>
   )
 }
 
