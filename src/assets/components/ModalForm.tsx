9c9c7efd9839dc957bdba81f98de7b1bb0d6741e
Creminin <n54318879@gmail.com>
2023-12-30 10:52:23 -0300

    Fixing Formik Component Children Error

--- src/assets/components/ModalForm.tsx
+++ src/assets/components/ModalForm.tsx
@@ -1,19 +1,21 @@
 import { Button, Flex } from "@chakra-ui/react"
 import { FormField } from "./Form"
-import { Formik } from "formik"
+import { Formik, Form } from "formik"
 
 export const ModalForm = () => {
     return(
         <Formik initialValues={{}} onSubmit={() => {}}>
-            <FormField Label="Número do cartão" InputType="number" PlaceHolder="**** ****"/>
-            <FormField Label="Nome do titular do cartão" InputType="name" PlaceHolder="Nome como esta no cartão"/>
+            <Form>
+                <FormField Label="Número do cartão" InputType="number" PlaceHolder="**** ****"/>
+                <FormField Label="Nome do titular do cartão" InputType="name" PlaceHolder="Nome como esta no cartão"/>
 
-            <Flex>
-                <FormField Label="Data de validade" InputType="date"/>
-                <FormField Label="CVV" InputType="number" PlaceHolder="***"/>
-            </Flex>
-            
-            <Button type="submit" w="100%">Adicionar Cartão</Button>
+                <Flex>
+                    <FormField Label="Data de validade" InputType="date"/>
+                    <FormField Label="CVV" InputType="number" PlaceHolder="***"/>
+                </Flex>
+                
+                <Button type="submit" w="100%">Adicionar Cartão</Button>
+            </Form>
         </Formik>
     )
 }
